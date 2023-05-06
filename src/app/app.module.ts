48a76599a15a8cc4c4391e66697d0fc4b7ee27da
erandaka.bandara <erandakaanupama@gmail.com>
2023-05-06 14:14:09 +0530

    feat: introduce basic routing
    (cherry picked from commit 4eb81c83b2ed1c412267ba3062d29edc504444a2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module'; // CLI imports 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HomeComponent,
+      AboutComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
